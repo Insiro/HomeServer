726dfae95a29c486126420a164519da86fdf03e7
= <=>
2020-01-22 21:59:16 +0900

    add : model : botskill db

--- CyphersChat/epic7/models.py
+++ CyphersChat/epic7/models.py
@@ -37,16 +37,19 @@ class user(models.Model):
     name = models.CharField(max_length=100)
     UPW = models.CharField(max_length=100)
     Signed = models.BooleanField(default=False)
+
     def __str__(self):
-        return self.UID +' : '+self.name
+        return self.UID + ' : '+self.name
+
 
 class notic(models.Model):
     name = models.CharField(max_length=200)
     writer = models.CharField(max_length=200, null=True)
     contents = models.TextField(max_length=4000, null=True)
     important = models.BooleanField(default=False)
-    linkName = models.CharField(max_length=20, null = True, blank = True)
+    linkName = models.CharField(max_length=20, null=True, blank=True)
     link = models.URLField(null=True, blank=True)
+
 
 class Heros(models.Model):
     SearchKey = models.CharField(max_length=100)
@@ -54,3 +57,11 @@ class Heros(models.Model):
     rarity = models.IntegerField()
     classType = models.CharField(max_length=20)
     element = models.CharField(max_length=10)
+
+
+class BotSkills(models.Model):
+    Skill = models.CharField(max_length=30)
+    target = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return self.Skill
