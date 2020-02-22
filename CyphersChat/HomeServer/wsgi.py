e132c5db44a719f68e8684c698ee10206a279365
PieSignal <insiro.me>
2020-02-22 17:15:23 +0900

    pot: change project name

--- CyphersChat/HomeServer/wsgi.py
+++ CyphersChat/HomeServer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CyphersChat project.
+WSGI config for HomeServer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CyphersChat.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeServer.settings")
 
 application = get_wsgi_application()
