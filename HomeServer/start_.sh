e7d6d203864c4e74d588b249fe4d69453d1f550d
Insiro <leeon@insiro.me>
2022-04-30 05:27:15 +0000

    update script for start

--- HomeServer/start_.sh
+++ HomeServer/start_.sh
@@ -1,1 +1,1 @@
-screen -dmS InsHome bash -c 'cd /home/leeon/leeon/HomeServer/HomeServer; gunicorn --bind 0.0.0.0:7100 HomeServer.wsgi:application'
+screen -dmS InsHome bash -c 'cd /home/ubuntu/HomeServer/HomeServer; gunicorn --bind 0.0.0.0:7100 HomeServer.wsgi:application'
